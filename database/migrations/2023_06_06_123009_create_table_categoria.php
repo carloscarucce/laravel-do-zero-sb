2a3ed954716c2b6317ffc3810f7d45fec531033c
Carlos Alberto Bertholdo Carucce <carloscarucce@gmail.com>
2023-06-06 10:18:12 -0300

    Estrutura do banco - Migrations

--- database/migrations/2023_06_06_123009_create_table_categoria.php
+++ database/migrations/2023_06_06_123009_create_table_categoria.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categorias', function (Blueprint $table) {
             $table->id();
             $table->string('descricao', 250);
-            $table->enum('status', ['ativo', 'inativo']);
+            $table->enum('status', ['ativo', 'inativo'])->default('ativo');
             $table->timestamps();
         });
     }
