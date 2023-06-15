d2e86c7b37b3c2d40259654ac8b47b9ba55ecee1
daniloasb <danilo_asb@yahoo.com.br>
2023-06-15 09:04:42 -0300

    150623 4

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome');   
+});
+
+
+Route::get('post', function () {
+    return view('index');   
 });
