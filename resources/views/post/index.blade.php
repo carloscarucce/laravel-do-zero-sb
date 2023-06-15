169bfbc0fbb51b3719457184ac2a563d37e163b5
daniloasb <danilo_asb@yahoo.com.br>
2023-06-15 10:44:42 -0300

    150323 6

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -6,11 +6,13 @@
     <div class="row">
         @foreach($posts as $post)
             <div class="col-6 col-md-3 mb-2">
-                <x-list-item
+                {{ $post->titulo }}
+                {{-- <x-list-item
                     :titulo="$post->titulo"
                     :conteudo="'Por: ' . $post->autor->name"
                     href="#"
                     :model="$post"/>
+                --}}
             </div>
         @endforeach
     </div>
