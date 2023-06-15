d89d5ce913e5fd68ec8b31dd5e77ab6c7c6bae69
daniloasb <danilo_asb@yahoo.com.br>
2023-06-15 08:42:13 -0300

    150623 2

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Categoria extends Model
 {
     use HasFactory;
+
+    /**
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'descricao', 'status',
+    ];
+
+    /**
+     * @return HasMany
+     */
+    public function posts()
+    {
+        return $this->hasMany(Post::class, 'categoria_id', 'id');
+    }
 }
--- app/Models/Comentario.php
+++ app/Models/Comentario.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comentario extends Model
 {
     use HasFactory;
+
+    /**
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'nome', 'conteudo', 'post_id',
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function post()
+    {
+        return $this->belongsTo(Post::class, 'post_id', 'id');
+    }
 }
