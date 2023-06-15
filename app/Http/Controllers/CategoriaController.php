bc0b51d6efdef1ec6276c5d49207aeb515c0550b
daniloasb <danilo_asb@yahoo.com.br>
2023-06-15 08:31:11 -0300

    160623 1

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categoria;
 use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
 {
-    //
+    public function index()
+    {
+        $categorias = Categoria::query()
+            ->where('status', 'ativo')
+            ->paginate(8);
+
+        return view('categoria.index', compact('categorias'));
+    }
 }
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::query()
+            ->with('autor')
+            ->paginate(8);
+
+        return view('post.index', compact('posts'));
+    }
 }
