a4153d19ff820f55cbf41bddd72458e270409937
daniloasb <danilo_asb@yahoo.com.br>
2023-06-15 08:45:48 -0300

    150623 3

---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,19 @@
+@extends('site')
+
+@section('title', 'Postagens')
+
+@section('contents')
+    <div class="row">
+        @foreach($posts as $post)
+            <div class="col-6 col-md-3 mb-2">
+                <x-list-item
+                    :titulo="$post->titulo"
+                    :conteudo="'Por: ' . $post->autor->name"
+                    href="#"
+                    :model="$post"/>
+            </div>
+        @endforeach
+    </div>
+
+    {{ $posts->withQueryString()->links() }}
+@endsection
