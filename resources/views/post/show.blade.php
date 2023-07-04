29cf22135e5cdcc37ab75f8f66adab57e1335dea
daniloasb <danilo_asb@yahoo.com.br>
2023-07-04 09:23:23 -0300

    04-07-23 2

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -13,21 +13,61 @@
         {{-- nl2br converte enter em <br>  --}}
     </div>
 
-    <div class="row">
-        <form method="post" action="" enctype="application/x-www-form-urlencoded">
-            <input 'type="text"' 
-        
-            <div id="campos"></div>
-        
-            <br/>
-            <button type="submit">
-                Salvar
-            </button>
-        </form>
-        
-     </div>
+    <hr/>
 
+    <h3>Comentários: </h3>
+    <form method="post" class="my-3">
+        @csrf
+        <input name="post_id" type="hidden" value="{{ $post->id }}"/>
 
+        <div class="row mb-3">
+            <div class="col-12">
+                <label class="control-label">Nome:</label>
+                <input name="nome" type="text" placeholder="Digite seu nome..." maxlength="250" class="form-control"/>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-10">
+                <textarea name="nome" placeholder="Comentário..." maxlength="500" class="form-control"></textarea>
+            </div>
+
+            <div class="col-2">
+                <button type="submit" class="btn btn-primary w-100 h-100">
+                    Enviar
+                </button>
+            </div>
+        </div>
+    </form>
+    <hr/>
+    <div id="comentarios"></div>
+
+    <script>
+        const carregarComentarios = function(url) {
+            const listaComentarios = document.getElementById('comentarios');
+
+            listaComentarios.innerHTML = 'Carregando comentários...';
+
+            fetch(url)
+                .then((response) => response.text())
+                .then(function (html) {
+                    listaComentarios.innerHTML = html;
+
+                    const links = listaComentarios.querySelector('#paginas_comentarios')
+                        .querySelectorAll('a.page-link');
+
+                    for (let i in links) {
+                        const link = links[i];
+                        link.onclick = (function(){
+                            carregarComentarios(this.href);
+                            return false;
+                        });
+                    }
+                });
+        };
+
+        carregarComentarios('{{ url("posts/{$post->id}/comentarios") }}');
+    </script>
 
 
 @endsection
