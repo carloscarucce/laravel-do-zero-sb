f36f68dfa910d52dbefb1e6de7b334b0c11f74ec
daniloasb <danilo_asb@yahoo.com.br>
2023-07-06 10:10:33 -0300

    06-07-2023 1

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -31,7 +31,7 @@ class PostController extends Controller
     {
         $comentarios = Comentario::query()
             ->where('post_id', $id)
-            ->paginate(1);
+            ->paginate(8);
         
         return view("post.comments-list", compact("comentarios"));
     }
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -1,22 +1,30 @@
 @extends('site')
 
-@section('title', 'Postagens')
+@section('title', $posts->titulo)
 
 @section('contents')
-    <div class="row">
-        {{ $posts->titulo }}
+
+    <h1>{{ $posts->titulo }}</h1>
+
+    <div class="mb-4">
+        Categoria: <span class="badge bg-warning">{{ $posts->categoria->descricao }}</span>
     </div>
 
-    <div class="row">
-        {!! nl2br(strip_tags($posts->conteudo)); !!}
+    <div>
+        {!! nl2br(strip_tags($posts->conteudo, '<p><br><string>')) !!}
+    </div>
 
-        {{-- nl2br converte enter em <br>  --}}
+    <div class="text-muted text-end mt-4">
+        Criado por:
+        {{ $posts->autor?->name ?? 'Desconhecido' }} -
+        Última atualização
+        {{ $posts->updated_at?->format('d/m/Y H:i:s') ?? $posts->created_at?->format('d/m/Y H:i:s') }}
     </div>
 
     <hr/>
 
     <h3>Comentários: </h3>
-    <form method="post" class="my-3">
+    <form id="form_comentario" method="post" class="my-3" action="{{ url('comentarios/save') }}">
         @csrf
         <input name="post_id" type="hidden" value="{{ $posts->id }}"/>
 
@@ -29,7 +37,7 @@
 
         <div class="row">
             <div class="col-10">
-                <textarea name="nome" placeholder="Comentário..." maxlength="500" class="form-control"></textarea>
+                <textarea name="conteudo" placeholder="Comentário..." maxlength="500" class="form-control"></textarea>
             </div>
 
             <div class="col-2">
@@ -46,7 +54,7 @@
         const carregarComentarios = function(url) {
             const listaComentarios = document.getElementById('comentarios');
 
-            listaComentarios.innerHTML = 'Carregando comentários...';
+            //listaComentarios.innerHTML = 'Carregando comentários...';
 
             fetch(url)
                 .then((response) => response.text())
@@ -66,8 +74,36 @@
                 });
         };
 
+        document.querySelector('#form_comentario').addEventListener('submit', function(e){
+            e.preventDefault();
+            const url = this.action;
+            const options = {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-Csrf-Token": this.querySelector('input[name="_token"]').value,
+                },
+                mode: "cors", // no-cors, *cors, same-origin
+                cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+                credentials: "same-origin", // include, *same-origin, omit
+                redirect: "follow",
+                body: JSON.stringify({
+                    "post_id": this.querySelector('input[name="post_id"]').value,
+                    "nome": this.querySelector('input[name="nome"]').value,
+                    "conteudo": this.querySelector('textarea[name="conteudo"]').value,
+                })
+            };
+
+            const onSuccess = function () {
+                carregarComentarios('{{ url("posts/{$posts->id}/comentarios") }}');
+            };
+            const onError = function() {
+                alert('Não foi possível enviar o comentário');
+            };
+
+            fetch(url, options).then(onSuccess, onError);
+        });
+
         carregarComentarios('{{ url("posts/{$posts->id}/comentarios") }}');
     </script>
-
-
 @endsection
