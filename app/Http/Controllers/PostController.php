afb493f4c8e51d5214a1b6df0766a24647509ef3
daniloasb <danilo_asb@yahoo.com.br>
2023-07-06 10:32:22 -0300

    06-07-2023 4

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -17,6 +17,60 @@ class PostController extends Controller
         return view('post.index', compact('posts'));
     }
 
+ 
+ 
+    public function create()
+    {
+        $post = new Post();
+        $categorias = Categoria::query()->orderBy('descricao')->get();
+
+        return view('post.form', compact('post', 'categorias'));
+    }
+
+    public function save(Request $request)
+    {
+        try {
+
+            DB::beginTransaction();
+
+            $post = Post::findOrNew($request->get('id'));
+            $post->fill($request->all());
+            $post->slug = Str::slug($post->titulo);
+            $post->save();
+
+            if ($request->hasFile('foto_capa')) {
+                /* @var $capa UploadedFile */
+                $capa = $request->files->get('foto_capa');
+
+                $arquivo = public_path('uploads/post-'.$post->id.'/capa.jpg');
+                $pasta = dirname($arquivo);
+
+                if (!is_dir($pasta)) {
+                    mkdir($pasta, recursive: true);
+                }
+
+                if (!is_writable($pasta)) {
+                    throw new Exception('A pasta não pode ser escrita');
+                }
+
+                $capa->move($pasta, 'capa.jpg');
+            }
+
+            DB::commit();
+            return redirect('posts/'.$post->slug);
+
+        } catch (Exception $exception) {
+            DB::rollBack();
+            return redirect()->back();
+        }
+    }
+
+ 
+ 
+ 
+ 
+ 
+ 
     public function show($slug)
     {
         $posts = Post::query()
